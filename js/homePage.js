b2b6bd88bbeb34d9ae8e0b0795c4a0ed57154bce
Hung Tran <hqtran@ucsd.edu>
2016-10-25 04:00:24 -0700

    Home Page Implemented
    Home Page given CSS and Javascript coding

---
+++ js/homePage.js
@@ -0,0 +1,56 @@
+'use strict';
+$(document).ready(function() {
+	initialize();
+})
+
+function initialize() {
+
+	$("#createText").on({
+		"mouseover": function() {
+			document.getElementById("createText").style.fontSize = "30px";
+		},
+		"mouseleave": function() {
+			document.getElementById("createText").style.fontSize = "26px";
+		}
+	});
+	$("#runText").on({
+		"mouseover": function() {
+			document.getElementById("runText").style.fontSize = "30px";
+		},
+		"mouseleave": function() {
+			document.getElementById("runText").style.fontSize = "26px";
+		}
+	});
+	$("#settingsText").on({
+		"mouseover": function() {
+			document.getElementById("settingsText").style.fontSize = "30px";
+		},
+		"mouseleave": function() {
+			document.getElementById("settingsText").style.fontSize = "26px";
+		}
+	});
+	$("#historyText").on({
+		"mouseover": function() {
+			document.getElementById("historyText").style.fontSize = "30px";
+		},
+		"mouseleave": function() {
+			document.getElementById("historyText").style.fontSize = "26px";
+		}
+	});
+	$("#helpText").on({
+		"mouseover": function() {
+			document.getElementById("helpText").style.fontSize = "30px";
+		},
+		"mouseleave": function() {
+			document.getElementById("helpText").style.fontSize = "26px";
+		}
+	});
+	$("#loginText").on({
+		"mouseover": function() {
+			document.getElementById("loginText").style.fontSize = "30px";
+		},
+		"mouseleave": function() {
+			document.getElementById("loginText").style.fontSize = "26px";
+		}
+	});
+}

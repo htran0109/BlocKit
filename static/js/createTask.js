ac65b51e10ebc78f8734707b7df7f14f16652f73
Hung Tran <hqtran@ucsd.edu>
2016-10-26 15:20:26 -0700

    Added Blocked websites list
    TODO: Checkboxes toggle on/off, add button functionality

---
+++ static/js/createTask.js
@@ -0,0 +1,19 @@
+/*For dropdown, when clicked, show websites blocked*/
+
+
+
+function showBlocked() {
+	document.getElementById("blockDropdown").classList.toggle("show");
+
+}
+
+$(".checkBox").click(function() {
+	if($(this).src == "../images/checkBox.png") {
+		$(this).src = "../images/emptyCheckBox.jpg";
+	}
+	else {
+		$(this).src = "../images/checkBox.png";
+	}
+});
+
+

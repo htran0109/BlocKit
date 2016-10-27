1ad5b39d55339cadf9e6ed4c8b35b9eff8081c67
Hung Tran <hqtran@ucsd.edu>
2016-10-26 17:15:29 -0700

    Finished Milestone
    Two pages work correctly

--- static/js/createTask.js
+++ static/js/createTask.js
@@ -1,23 +1,32 @@
 /*For dropdown, when clicked, show websites blocked*/
 
-
+$(document).ready(function() {
+	initialize();
+})
 
 function showBlocked() {
 	document.getElementById("blockDropdown").classList.toggle("show");
 
 }
+function initialize() {
+	$(".inner-content").click(function(event) {
+		var fullCheck = "../images/checkBox.png";
+		var emptyCheck = "../images/emptyCheckBox.png";
+		
+		var target = (event.target.id);
+		$this = (target + " img");
+		if($this.attr('src') == fullCheck) { 
+			$this.attr('src', emptyCheck);
+		}
+		else {
+			$this.attr('src', fullCheck);
+		}
+	});
 
-$(".checkBox").click(function() {
-	if($(this).src == "../images/checkBox.png") {
-		$(this).src = "../images/emptyCheckBox.jpg";
-	}
-	else {
-		$(this).src = "../images/checkBox.png";
-	}
-});
-
-$("#webAdd").click(function() {
+	$("#webAdd").click(function() {
 	$('#websiteText').val("https://");
-})
+	});
+
+}
 
 

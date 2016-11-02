db329df69159509f0b974f154b3c1adf76b6c570
Hung Tran <htran0109@gmail.com>
2016-11-01 21:32:35 -0700

    Broke history page, will fix with handlebars tonight

--- routes/taskScreen.js
+++ routes/taskScreen.js
@@ -1,14 +1,12 @@
 
-var data = require('../static/json_data/data.json');
+/*For dropdown, when clicked, show websites blocked*/
+
+var data = require('../static/json_data/completed.json');
 
 exports.view = function(req, res){
-	//grab the taskNumber to be shown
-	console.log(req.params.taskNumber);
-	var taskNumber = parseInt(req.params.taskNumber,10);
-	console.log(taskNumber);
 	console.log(data);
-	console.log(data.tasks[taskNumber]);
-	res.render('taskScreen', data.tasks[taskNumber]);
-	
-	
+	res.render('history', data);
 };
+
+
+

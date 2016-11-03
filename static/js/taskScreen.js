9961a617f47aee488ff074f54c426618c4f46e65
Hung Tran <htran0109@gmail.com>
2016-11-03 16:19:19 -0700

    Added timer functionality

--- static/js/taskScreen.js
+++ static/js/taskScreen.js
@@ -1,4 +1,5 @@
-/*For dropdown, when clicked, show websites blocked*/
+
+var timer = setInterval(timeTask, 60000);
 
 $(document).ready(function() {
 	initialize();
@@ -8,13 +9,32 @@ function showBlocked() {
 	document.getElementById("blockDropdown").classList.toggle("show");
 
 }
+
+
+
+function timeTask() {
+	
+
+	var hours = $("#hours").val();
+    var hoursInt = parseInt(hours);
+    var minutes = $("#minutes").val();
+    console.log(minutes);
+    var minutesInt = parseInt(minutes);
+    var totalMinutes = hoursInt * 60 + minutesInt;
+    totalMinutes--;
+    if(totalMinutes >= 0) {
+      $('#hours').val(Math.floor(totalMinutes / 60));
+      $('#minutes').val(totalMinutes % 60);
+    }
+}
+
 function initialize() {
 
 
 	$("#webAdd").click(function() {
 	$('#websiteText').val("https://");
 	});
-
 }
+
 
 
